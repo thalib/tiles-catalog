d01ce6a881b88e2a49ca56bfafe27a9b877fa6f5
Mohamed Thalib <hmthalib@gmail.com>
2023-04-04 20:37:03 +0530

    :100:  working tile_wall.py  updated + more bg and tiles

--- tile_wall.py
+++ tile_wall.py
@@ -30,7 +30,7 @@ def runCmd(cmd: str):
   print(f" ✅  {cmd}")
   ret = os.system(cmd)
   if not ret == 0:
-     print("#Error: " + ret)
+     print("#Error: " + str(ret))
      exit(255)
 ###########################################
 def tile_level_1(tile_data):
@@ -47,22 +47,33 @@ def tile_level_2(model):
   file_out_final = f"out/image/{model}"
   cmd_args = " -duplicate 8 -tile x1 -geometry +0+0 -gravity southwest -resize 2000x950^ -crop 1920x890+0+0 "
   cmd = f"magick montage {file_out}-L1.png {cmd_args} {file_out}-L2.png"
+  cmd = f"magick montage {file_out}-L1.png {cmd_args} {file_out}-L2.png"
   runCmd(cmd)
 
-def tile_level_3(model, background):
+def tile_level_3(model, tile_data):
   file_out = f"out/temp/{model}"
-  file_out_final = f"out/image/wall/{model}"
-  cmd = f"magick -size 1920x1080 xc:darkgrey  {file_out}-L2.png -gravity northwest -geometry +0+0 -composite {background} -composite {file_out_final}.png"
-  runCmd(cmd)
-   
-def tile_level_4(model, tile_data):
-  file_out = f"out/temp/{model}"
+  offset_x = 50
+  offset_y = 80
   file_input = list(set(tile_data['row']))
-  file_input = " ".join(file_input)
-  file_base = f"out/image/{model}"
-  print(file_input)
-  cmd_args = f" -tile 3x -border 1 -geometry +15+0"
-  cmd = f"magick {file_base}.png asset/tile/wall/1002HL.png  -border 5 -geometry 300x150+100+900 -composite {file_out}-L4.png"
+  cmd_args = ""
+  cmd_args += f" -gravity northwest -fill black -pointsize 35 -annotate +{offset_x}+15 Model:{model} "
+  cmd_args += f" -gravity north -fill black -pointsize 35 -annotate +{offset_x}+15 www.deltaware.in "
+  cmd_args += f" -gravity northeast -fill black -pointsize 35 -annotate +{offset_x}+15 Whatsapp:9940198130 "
+  for item in file_input:
+    cmd_args += f" {item} -border 5 -geometry 300x180+{offset_x}+{offset_y} -gravity northwest -composite "
+    offset_x += 300
+    offset_y += 5
+  
+  cmd = f"magick  -size 1920x280 xc:white {cmd_args} {file_out}-L3.png"
+  runCmd(cmd)
+  
+def tile_level_4(model, background):
+  file_out = f"out/image/wall/{model}"  
+  file_in = f"out/temp/{model}"
+  cmd_args = f" -size 1920x1360 xc:darkgrey {file_in}-L2.png -gravity northwest -geometry +0+0 -composite {background} -composite "
+  cmd_args += " asset/logo.png -gravity northwest -geometry +50+20 -composite "
+  cmd_args += f" {file_in}-L3.png -gravity southeast -geometry +0+0 -composite "
+  cmd = f"magick {cmd_args} {file_out}.png"
   runCmd(cmd)
   
 ###########################################
@@ -75,7 +86,7 @@ PATH_BG = "asset/bg/outdoor"
 os.makedirs("out/temp", exist_ok = True)
 os.makedirs(PATH_OUT, exist_ok = True)
 
-json_data = json.loads(loadFile("model3.json"))
+json_data = json.loads(loadFile("model_wall_tiles.json"))
 
 bg_list = os.listdir("asset/bg/wall")
 
@@ -92,6 +103,5 @@ for row in json_data:
     print(f"  Processing: {model} with background {bg}")  
     tile_level_1(row)
     tile_level_2(model)
-    tile_level_3(model, file_bg)
-    #tile_level_4(model, row) <- todo
-  
+    tile_level_3(model, row)
+    tile_level_4(model, file_bg)
